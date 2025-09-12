dc3e3afe108cd322f34ff24a3984fe98dca636fb
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2025-09-12 23:00:04 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -67,7 +67,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "334b66d5ec8e70d1df55850eb0724755
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "6b7607fa48174e9b6a086f728e90e2e5",
+"flutter_bootstrap.js": "bf5193cf5d5be6f27fb6940bf7104d82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
